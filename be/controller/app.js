9014365d12b8c43d16094145d99a27395ce4dd37
simon-chong <clemencewong.20@ichat.sp.edu.sg>
2021-11-06 14:31:33 +0800

    add backend test

---
+++ be/controller/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+//var bodyParser = require("body-parser");
+var app = express();
+var port = 3000;
+var hostname = "localhost";
+//var logger = require("../middleware/mid");
+//Implement the User model inside the index.js file.
+//const be = require("../services/be");
+//var request = require("request");
+
+var urlencodedParser = express.urlencoded({ extended: true });
+app.use(urlencodedParser); //Attach body-parser middleware (openning letter)
+app.use(express.json()); //Parse json data (output)
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post('/Hello', (req, res, next) => {
+    res.send({message:"hello"});
+})
