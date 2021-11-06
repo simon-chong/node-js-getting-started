e8539eabaf396f0e079f322ac1f4c7b8b46ac507
simon-chong <clemencewong.20@ichat.sp.edu.sg>
2021-11-06 15:16:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
+//const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = require("./app");
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
