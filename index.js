b8b3ef7699eb7ce047a9951e761fbc141c2f048e
simon-chong <clemencewong.20@ichat.sp.edu.sg>
2021-11-06 15:12:51 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = require("./controller/app");
+const app = require("./app");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
