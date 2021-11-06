6a235c7250107bb763fdadae323a05856b800cdb
simon-chong <clemencewong.20@ichat.sp.edu.sg>
2021-11-06 15:11:18 +0800

    try again

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = require("./controller/app");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
